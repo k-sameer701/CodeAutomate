4cd4fbb7564b916f82e4a7dad446bf2c8bcef46f
k-sameer701 <sameer2001kr@gmail.com>
2024-11-02 11:05:43 +0530

    messages added

--- CodeAutomate/user_profile/views.py
+++ CodeAutomate/user_profile/views.py
@@ -1,36 +1,32 @@
 from django.shortcuts import render
 
 def index(request):
-    # if request.method == 'POST':
-    #     text_input = request.POST.get('text_input')
-    #     context = {'text_input': text_input}
-    # else:
-    #     context = {}
-    # return render(request, 'my_app/index.html', context)
-    # text_input = None
-    # icon_1 = None
-
-    # if request.method == "POST":
-    #     text_input = request.POST.get('text_input')
-    #     icon_1 = request.POST.get('icon_1')
-
-    # return render(request, 'my_app/index.html', {
-    #     'text_input': text_input,
-    #     'icon_1': icon_1,
-    # })
+    
 
     # Initialize context dictionary
     context = {}
 
     if request.method == 'POST':
-        # Get the textarea input
-        text_input = request.POST.get('text_input')
+        # Get the firstName input
+        firstName = request.POST.get('firstName')
+
+        # Get the email input
+        email = request.POST.get('email')
+
+        # Get the website input
+        website = request.POST.get('website')
+
+        # Get the bio input
+        bio = request.POST.get('bio')
 
         # Get the value of the selected radio button
         icon_1 = request.POST.get('icon_1')
 
         # Update the context dictionary with the form data
-        context['text_input'] = text_input
+        context['firstName'] = firstName
+        context['email'] = email
+        context['website'] = website
+        context['bio'] = bio
         context['icon_1'] = icon_1
 
     # Render the template with the context
