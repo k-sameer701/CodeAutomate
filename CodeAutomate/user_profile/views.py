fc0266d1cb7a3f494a259f894e78be7d06865c50
k-sameer701 <sameer2001kr@gmail.com>
2024-11-02 10:47:32 +0530

    style done

--- CodeAutomate/user_profile/views.py
+++ CodeAutomate/user_profile/views.py
@@ -8,15 +8,15 @@ def index(request):
     #     context = {}
     # return render(request, 'my_app/index.html', context)
     # text_input = None
-    # fav_language = None
+    # icon_1 = None
 
     # if request.method == "POST":
     #     text_input = request.POST.get('text_input')
-    #     fav_language = request.POST.get('fav_language')
+    #     icon_1 = request.POST.get('icon_1')
 
     # return render(request, 'my_app/index.html', {
     #     'text_input': text_input,
-    #     'fav_language': fav_language,
+    #     'icon_1': icon_1,
     # })
 
     # Initialize context dictionary
@@ -27,11 +27,11 @@ def index(request):
         text_input = request.POST.get('text_input')
 
         # Get the value of the selected radio button
-        fav_language = request.POST.get('fav_language')
+        icon_1 = request.POST.get('icon_1')
 
         # Update the context dictionary with the form data
         context['text_input'] = text_input
-        context['fav_language'] = fav_language
+        context['icon_1'] = icon_1
 
     # Render the template with the context
     return render(request, 'user_profile/index.html', context)
