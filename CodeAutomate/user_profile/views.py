3657fd05914cf52d8d894f33ac98a759f19e5120
k-sameer701 <sameer2001kr@gmail.com>
2024-11-03 23:01:21 +0530

    icons done

--- CodeAutomate/user_profile/views.py
+++ CodeAutomate/user_profile/views.py
@@ -81,8 +81,48 @@ def index(request):
         mobile_3 = request.POST.get('mobile_3')
         mobile_4 = request.POST.get('mobile_4')
 
-        # Get the value of the selected radio button
-        icon_1 = request.POST.get('icon_1')
+        # AI-ML
+        ai_1 = request.POST.get('ai_1')
+        ai_2 = request.POST.get('ai_2')
+        ai_3 = request.POST.get('ai_3')
+        ai_4 = request.POST.get('ai_4')
+        ai_5 = request.POST.get('ai_5')
+        ai_6 = request.POST.get('ai_6')
+
+        # DATABASE
+        db_1 = request.POST.get('db_1')
+        db_2 = request.POST.get('db_2')
+        db_3 = request.POST.get('db_3')
+        db_4 = request.POST.get('db_4')
+        db_5 = request.POST.get('db_5')
+        db_6 = request.POST.get('db_6')
+        db_7 = request.POST.get('db_7')
+        db_8 = request.POST.get('db_8')
+        db_9 = request.POST.get('db_9')
+        db_10 = request.POST.get('db_10')
+        db_11 = request.POST.get('db_11')
+
+        # DATA-VISULIZATION
+        dv_1 = request.POST.get('dv_1')
+        dv_2 = request.POST.get('dv_2')
+        dv_3 = request.POST.get('dv_3')
+        dv_4 = request.POST.get('dv_4')
+        dv_5 = request.POST.get('dv_5')
+
+        # DEVOPS
+        do_1 = request.POST.get('do_1')
+        do_2 = request.POST.get('do_2')
+        do_3 = request.POST.get('do_3')
+        do_4 = request.POST.get('do_4')
+        do_5 = request.POST.get('do_5')
+        do_6 = request.POST.get('do_6')
+        do_7 = request.POST.get('do_7')
+
+        # BAS
+        bas_1 = request.POST.get('bas_1')
+        bas_2 = request.POST.get('bas_2')
+        bas_3 = request.POST.get('bas_3')
+        bas_4 = request.POST.get('bas_4')
 
         # Update the context dictionary with the form data
         context['fullName'] = fullName
@@ -139,6 +179,49 @@ def index(request):
         context['mobile_2'] = mobile_2
         context['mobile_3'] = mobile_3
         context['mobile_4'] = mobile_4
+
+        # AI-ML
+        context['ai_1'] = ai_1
+        context['ai_2'] = ai_2
+        context['ai_3'] = ai_3
+        context['ai_4'] = ai_4
+        context['ai_5'] = ai_5
+        context['ai_6'] = ai_6
+
+        # DATABASE
+        context['db_1'] = db_1
+        context['db_2'] = db_2
+        context['db_3'] = db_3
+        context['db_4'] = db_4
+        context['db_5'] = db_5
+        context['db_6'] = db_6
+        context['db_7'] = db_7
+        context['db_8'] = db_8
+        context['db_9'] = db_9
+        context['db_10'] = db_10
+        context['db_11'] = db_11
+
+        # DATA-VISULISATION
+        context['dv_1'] = dv_1
+        context['dv_2'] = dv_2
+        context['dv_3'] = dv_3
+        context['dv_4'] = dv_4
+        context['dv_5'] = dv_5
+
+        # DEVOPS
+        context['do_1'] = do_1
+        context['do_2'] = do_2
+        context['do_3'] = do_3
+        context['do_4'] = do_4
+        context['do_5'] = do_5
+        context['do_6'] = do_6
+        context['do_7'] = do_7
+
+        # BAS
+        context['bas_1'] = bas_1
+        context['bas_2'] = bas_2
+        context['bas_3'] = bas_3
+        context['bas_4'] = bas_4
 
     # Render the template with the context
     return render(request, 'user_profile/index.html', context)
