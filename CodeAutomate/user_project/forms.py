76b67a7d3f375b51ef6ea127de5576e2ac351018
k-sameer701 <sameer2001kr@gmail.com>
2025-02-02 15:29:06 +0530

    project logic done

--- CodeAutomate/user_project/forms.py
+++ CodeAutomate/user_project/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
-class StepForm(forms.Form):
-    step = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter a step'}), required=True)
+class MultiStepForm(forms.Form):
+    usage_steps = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Usage Step'}), required=False)
+    feature_steps = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Feature Step'}), required=False)
+    contribution_steps = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Contribution Step'}), required=False)
+    installation_steps = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Installation Step'}), required=False)
--- CodeAutomate/user_project/views.py
+++ CodeAutomate/user_project/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render
-from .forms import StepForm
+from .forms import MultiStepForm
 
 def user_project(request):
     context = {}
 
     if request.method == 'POST':
+        github_UserId = request.POST.get('github_UserId')
         project_title = request.POST.get('project_title')
-        steps = request.POST.getlist('steps')  # Corrected name for list input
-        
+        userName = request.POST.get('userName')
+        project_description = request.POST.get('project_description')
+        app_icon_url = request.POST.get('app_icon_url')
+        screenshots_url = request.POST.get('screenshots_url')
+        paypal_id = request.POST.get('paypal_id')
+        buy_me_coffee = request.POST.get('buy_me_coffee')
+        # steps = request.POST.getlist('steps')  # Corrected name for list input
+        usage_steps = request.POST.getlist('usage_steps[]')  # Get all Usage steps
+        feature_steps = request.POST.getlist('feature_steps[]')  # Get all Feature steps
+        contribution_steps = request.POST.getlist('contribution_steps[]')  # Get all Contribution steps
+        installation_steps = request.POST.getlist('installation_steps[]')
+
+        context['github_UserId'] = github_UserId
         context['project_title'] = project_title
-        context['steps'] = steps
+        context['userName'] = userName
+        context['project_description'] = project_description
+        context['app_icon_url'] = app_icon_url
+        context['screenshots_url'] = screenshots_url
+        context['paypal_id'] = paypal_id
+        context['buy_me_coffee'] = buy_me_coffee
+        context['usage_steps'] = usage_steps
+        context['feature_steps'] = feature_steps
+        context['contribution_steps'] = contribution_steps
+        context['installation_steps'] = installation_steps
 
         return render(request, 'user_project.html', context)  # Fixed passing context
 
-    form = StepForm()
+    form = MultiStepForm()
     context['form'] = form  # Include the form in context
     return render(request, 'user_project.html', context)  # Fixed passing context
